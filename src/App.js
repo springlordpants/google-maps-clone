96407ace551f978ba13d6a4fbcdb36bacd6a8784
springlordpants <springlordpants@gmail.com>
2020-11-23 14:29:16 -0800

    adding useState, token, and accessing both

--- src/App.js
+++ src/App.js
@@ -1,7 +1,27 @@
-import React from "react"
-
+import React, { useState } from "react"
+import ReactMapGL from "react-map-gl"
 // import GlobalStyles from "./styles/GlobalStyles.css"
 
 export default function App() {
-  return <div>map</div>
+  const [viewport, setViewport] = useState({
+    latitude: 34.052235,
+    longitude: -118.243683,
+    width: "100vw",
+    height: "100vh",
+    zoom: 10,
+  })
+
+  return (
+    <div>
+      <ReactMapGL
+        {...viewport}
+        mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_TOKEN}
+        onViewportChange={(viewport) => {
+          setViewport(viewport)
+        }}
+      >
+        Markers here
+      </ReactMapGL>
+    </div>
+  )
 }
