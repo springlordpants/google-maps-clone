cc835fc98c3a2792a04aed07025b5647d0e71923
springlordpants <springlordpants@gmail.com>
2020-11-23 17:48:46 -0800

    reconfigured for Mapbox

--- src/App.js
+++ src/App.js
@@ -1,16 +1,26 @@
-import React from "react"
-import WrappedMap from "./comoponents/Map"
-import "./styles/GlobalStyles.css"
+import React, { useState } from "react"
+import ReactMapGL from "react-map-gl"
 
 export default function App() {
+  const [viewport, setViewport] = useState({
+    latitude: 34.052235,
+    longitude: -118.243683,
+    height: "100vh",
+    width: "100vw",
+    zoom: 10,
+  })
   return (
     <div>
-      <WrappedMap
-        googleMapURL={`https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=${process.env.REACT_APP_GOOGLE_KEY}`}
-        loadingElement={<div style={{ height: "100%" }} />}
-        containerElement={<div style={{ height: "100%" }} />}
-        mapElement={<div style={{ height: "100%" }} />}
-      />
+      <ReactMapGL
+        {...viewport}
+        mapboxApiAccessToken={process.env.REACT_APP_MAP_KEY}
+        mapStyle="mapbox://styles/springlordpants/ckhv4s2y101ms19pl30nuoiwr"
+        onViewportChange={(viewport) => {
+          setViewport(viewport)
+        }}
+      >
+        markers here
+      </ReactMapGL>
     </div>
   )
 }
